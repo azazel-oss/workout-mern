ad313aa3e35760939e1b3ac238f8ea564a229114
Asad Mahmood <asadmhmd1234@gmail.com>
2022-08-01 14:46:34 +0530

    Connect to the database and add mongoose

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+// connect to database
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.once("open", () => console.log("Connection established with the database"));
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 const workoutRouter = require("./routes/workouts");
 
