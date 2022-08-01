4a0170edc0887d92df0069b73ec4638123558c28
Asad Mahmood <asadmhmd1234@gmail.com>
2022-08-02 01:59:24 +0530

    Add CORS to the app for development server

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -21,6 +22,7 @@ const workoutRouter = require("./routes/workouts");
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/api/workouts", workoutRouter);
 
