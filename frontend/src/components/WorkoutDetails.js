f4c798c9359413b7a0222aff148306ef2cc24ca1
Asad Mahmood <asadmhmd1234@gmail.com>
2022-08-02 14:33:12 +0530

    Add delete data action to the dispatch reducer function

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,18 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+  async function handleClick() {
+    const response = await fetch(
+      "http://localhost:8080/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await response.json();
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json.workout });
+    }
+  }
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -9,6 +23,7 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Reps: {workout.reps}</strong>
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   );
 };
